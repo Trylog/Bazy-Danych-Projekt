fa3c1913329c0f508b68e2052b74af0ad5a301aa
Michał Bernacki-Janson <94766391+Trylog@users.noreply.github.com>
2023-11-15 10:58:25 +0100

    added removed_users, FK fix

--- Create.sql
+++ Create.sql
@@ -7,11 +7,17 @@ CREATE TABLE messages(id INT unsigned NOT NULL PRIMARY KEY,
 CREATE TABLE users(id INT unsigned not null primary key,
 					first_name varchar(32),
                     last_name varchar(32),
+                    avatar mediumblob,
                     status enum('active', 'not active'));
+CREATE TABLE removed_users(id INT unsigned not null auto_increment primary key,
+                            first_name varchar(32),
+                            last_name varchar(32));
+
 CREATE TABLE conversations(id int unsigned not null primary key,
 							name varchar(32),
                             creation_date date,
                             invitation enum('yes', 'no'),
+                            avatar mediumblob,
                             admin_id int unsigned);
 CREATE TABLE conversation_members(user_id int unsigned not null,
 									conversation_id int unsigned not null);
@@ -24,9 +30,11 @@ CREATE TABLE interactions(id int unsigned not null primary key,
 
 
 ALTER TABLE messages ADD constraint FK_conversation_id foreign key (conversation_id)
-										references conversation(id);
+										references conversations(id);
 ALTER TABLE messages ADD constraint FK_user_id foreign key (user_id)
 										references users(id);
+ALTER TABLE messages ADD constraint FK_removed_user_id foreign key (user_id)
+										references removed_users(id);
 ALTER TABLE messages ADD constraint FK_answer_to_id foreign key (answer_to_id)
 										references messages(id);
 
@@ -35,13 +43,15 @@ ALTER TABLE conversation_members ADD constraint FK_user_id foreign key (user_id)
 ALTER TABLE conversation_members ADD constraint FK_conversation_id foreign key (conversation_id)
 										references conversations(id);
 
-ALTER TABLE conversation_members ADD constraint FK_user_id foreign key (user_id)
-										references users(id);
-ALTER TABLE conversation_members ADD constraint FK_conversation_id foreign key (conversation_id)
-										references conversations(id);
-
 ALTER TABLE interactions ADD constraint FK_user_id foreign key (user_id)
 								references users(id);
+ALTER TABLE interactions ADD constraint FK_removed_user_id foreign key (user_id)
+								references removed_users(id);
 ALTER TABLE interactions ADD constraint FK_message_id foreign key (message_id)
 								references messages(id);
+
+ALTER TABLE moderators ADD constraint FK_user_id foreign key (user_id)
+                                references users(id);
+ALTER TABLE moderators ADD constraint FK_conversation_id foreign key (conversation_id)
+                                references conversations(id);
 
