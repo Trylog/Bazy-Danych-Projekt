e67b72f1d5e415de0e93e0dc3999b30eccc295df
jakub <jkl.school6@gmail.com>
2023-12-10 21:24:31 +0100

    stworzony trigger dodajacy do removed_user

--- Triggers.sql
+++ Triggers.sql
@@ -1,1 +1,11 @@
-CREATE TRIGGER
+# pomysly na trigger:
+# 1. zmiana statusu aktywnosci uzytkownika po zalogowaniu
+# 2. dodatkowa tabela przechowujaca informacje o edycji wiadomosci
+# 3. dodatkowa tabela przechowujaca informacje o edycji danych konwersacji
+CREATE TRIGGER USER_DEL AFTER DELETE ON messengerdatabase.users
+    FOR EACH ROW
+    BEGIN
+        INSERT INTO messengerdatabase.removed_users (id, first_name, last_name)
+        VALUES (old.id, old.first_name, old.last_name);
+    END;
+
